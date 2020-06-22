885d1da3f5f5628c315277bb6e6a254d1a91971c
Sebastian Vogel <sebastian.e.vogel@live.com>
2020-06-22 19:43:10 -0300

    added typical-react library and styled aboutMe component

--- src/components/aboutMe/index.js
+++ src/components/aboutMe/index.js
@@ -1,12 +1,33 @@
 import React from "react";
 import "./styles.css";
+import Typical from "react-typical";
 
 const AboutMe = () => {
- 
+  const img =
+    "https://images.unsplash.com/photo-1555527127-5d23f6f8e154?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80";
   return (
     <section className="profile">
       <div className="container-profile">
-        <img src="#" className="photo" alt="profile" />
+        <img src={img} className="photo" alt="profile" />
+        <p className="profile-title">
+          Soy{" "}
+          <Typical
+            steps={[
+              "Autodidacta",
+              1000,
+              "Developer",
+              1000,
+              "Front End-Web",
+              1000,
+              "Entusiasta",
+              1000,
+              "Sebastian!",
+              1000
+            ]}
+            loop={Infinity}
+            wrapper="b"
+          />
+        </p>
         <p className="profile-description">
           De manera autodidacta adquirí el conocimiento en los lenguajes de
           programación después mencionados, ya que estoy en constante
